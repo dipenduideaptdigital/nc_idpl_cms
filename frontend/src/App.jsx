2ce9d4481735080f8b2da72ea9589c1888a4e0a0
Dipendu Mahata <dipendu.ideaptdigital@gmail.com>
2026-06-23 20:33:23 +0530

    Implement Admin invite and document problem solve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 /* Layouts */
@@ -18,8 +18,10 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Blog from './pages/Blog';
 import BlogDetail from './pages/BlogDetail';
+import AdminSetup from './pages/AdminSetup';
 
 /* Admin Pages */
+import Dashboard from './pages/admin/Dashboard';
 import HomeCustomization from './pages/admin/HomeCustomization';
 import PageList from './pages/admin/pages/PageList';
 import PageEditor from './pages/admin/pages/PageEditor';
@@ -28,6 +30,11 @@ import ContactFormEditor from './pages/admin/contact-forms/ContactFormEditor';
 import BlogList from './pages/admin/blogs/BlogList';
 import BlogEditor from './pages/admin/blogs/BlogEditor';
 import TaxonomyManager from './pages/admin/blogs/TaxonomyManager';
+import SettingsLayout from './components/layout/SettingsLayout';
+import GeneralSettings from './pages/admin/settings/GeneralSettings';
+import RolesList from './pages/admin/roles/RolesList';
+import RoleEditor from './pages/admin/roles/RoleEditor';
+import UsersList from './pages/admin/users/UsersList';
 
 function App() {
   return (
@@ -36,9 +43,12 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/admin-setup" element={<AdminSetup />} />
           
           {/* Admin Routes */}
           <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
             <Route path="home-customization" element={<HomeCustomization />} />
             <Route path="pages" element={<PageList />} />
             <Route path="pages/create" element={<PageEditor />} />
@@ -50,6 +60,20 @@ function App() {
             <Route path="blogs/create" element={<BlogEditor />} />
             <Route path="blogs/edit/:id" element={<BlogEditor />} />
             <Route path="blogs/taxonomies" element={<TaxonomyManager />} />
+            <Route path="settings" element={<SettingsLayout />}>
+              <Route path="general" element={<GeneralSettings />} />
+              <Route path="users" element={<UsersList />} />
+              <Route path="roles" element={<RolesList />} />
+              <Route path="roles/create" element={<RoleEditor />} />
+              <Route path="roles/edit/:id" element={<RoleEditor />} />
+            </Route>
+          </Route>
+
+          {/* Public Routes - Landing Pages */}
+          <Route element={<LandingLayout />}>
+            <Route path="/hero-preview" element={<LandingReference />} />
+            <Route path="/preview/:token" element={<PreviewPage />} /> 
+            <Route path="/*" element={<DynamicPage />} />
           </Route>
 
           {/* Public Routes - Main Pages */}
@@ -58,13 +82,6 @@ function App() {
             <Route path="/hero-preview-2" element={<LandingReference2 />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:slug" element={<BlogDetail />} />
-          </Route>
-
-          {/* Public Routes - Landing Pages */}
-          <Route element={<LandingLayout />}>
-            <Route path="/hero-preview" element={<LandingReference />} />
-            <Route path="/preview/:token" element={<PreviewPage />} /> 
-            <Route path="/*" element={<DynamicPage />} />
           </Route>
           
         </Routes>
